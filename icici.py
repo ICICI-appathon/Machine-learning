f865676727c104b0d6f65b4cccb9bde08a7a2489
Sukhad <sukhad.anand@gmail.com>
2017-04-08 23:30:31 +0530

    added function

--- icici.py
+++ icici.py
@@ -4,10 +4,9 @@ from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import Imputer
-dataset=pd.read_csv('/home/sukhad/Downloads/Supporting files/Data/derived_features_validated.csv')
+dataset=pd.read_csv('')
 data=[]
 coloumns=list(dataset)
-print coloumns
 labels=[]
 for index,row in dataset.iterrows():
 	list=[]
@@ -23,6 +22,7 @@ clf=AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
                          algorithm="SAMME",
                          n_estimators=200)
 clf.fit(data,labels)
-a=clf.predict(data)
+def fun(data):
+	a=clf.predict(data)
 
 
