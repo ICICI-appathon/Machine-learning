ecefb868e240703965a93da2e5a19052caa9baac
Sukhad <sukhad.anand@gmail.com>
2017-04-12 03:30:28 +0530

    final1

--- luxury_check.py
+++ luxury_check.py
@@ -2,7 +2,7 @@ import pandas
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 import pickle
-def luxury() :
+def luxuryr() :
 	filename='luxury_check.sav'
 	clf = pickle.load(open(filename, 'rb'))
 	data1=[0.986021972,0.518867925,0.963674868,0.743931132,0.613207547,0.905005093]
